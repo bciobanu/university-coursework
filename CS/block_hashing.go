0df55aa27bff6d925b171212619cd1b950a8d377
Bogdan Ciobanu <11034715+bciobanu@users.noreply.github.com>
2020-03-21 13:00:27 +0200

    Finish Crypto homework

--- CS/block_hashing.go
+++ CS/block_hashing.go
@@ -6,7 +6,9 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"log"
+	"math/rand"
 	"reflect"
+	"strings"
 )
 
 type BlockHeader struct {
@@ -48,8 +50,8 @@ func hexToBin(str string) []byte {
 	return dst[:n]
 }
 
-func getRawBytes(header *BlockHeader) []byte {
-	r := reflect.ValueOf(*header)
+func getRawBytes(header BlockHeader) []byte {
+	r := reflect.ValueOf(header)
 	result := []byte{}
 	for i := 0; i < r.NumField(); i++ {
 		field := r.Field(i)
@@ -59,13 +61,12 @@ func getRawBytes(header *BlockHeader) []byte {
 		} else {
 			fieldBytes = hexToBin(field.Interface().(string))
 		}
-		log.Println(hex.EncodeToString(fieldBytes))
 		result = append(result, fieldBytes...)
 	}
 	return result
 }
 
-func computeHash(header *BlockHeader) string {
+func computeHash(header BlockHeader) string {
 	rawBytes := getRawBytes(header)
 	innerHash := sha256.Sum256(rawBytes)
 	hash := sha256.Sum256(innerHash[:])
@@ -73,7 +74,7 @@ func computeHash(header *BlockHeader) string {
 	return strToLittleEndian(hexStr)
 }
 
-func main() {
+func testExampleHeader() {
 	header := BlockHeader{
 		Version:    0x3fff0000,
 		PrevBlock:  "0000000000000000000140ac4688aea45aacbe7caf6aaca46f16acd93e1064c3",
@@ -82,5 +83,92 @@ func main() {
 		Bits:       0x17110119,
 		Nonce:      0x20184C38,
 	}
-	log.Println(computeHash(&header))
+	hash := computeHash(header)
+	if hash != "0000000000000000000d493c3c1b91c8059c6b0838e7e68fbcf8f8382606b82c" {
+		log.Fatal("Example block hash failed: ", hash)
+	}
+}
+
+func getDifficulty(nBits uint32) string {
+	// https://bitcoin.org/en/developer-reference#target-nbits
+	// https://bitcoin.stackexchange.com/questions/71578/understanding-bits-and-difficulty-in-a-block-header
+	const NumMantissaBits = 32 - 8
+	const MantissaMask = (1 << NumMantissaBits) - 1
+
+	exponent := 2 * ((nBits >> NumMantissaBits) - 3)
+	mantissa := uintToBin(nBits & MantissaMask)
+	mantissaStr := hex.EncodeToString(mantissa)
+	mantissaStr = strToLittleEndian(mantissaStr)
+	mantissaStr = mantissaStr + strings.Repeat("0", int(exponent))
+	mantissaStr = strings.Repeat("0", 64-len(mantissaStr)) + mantissaStr
+	return mantissaStr
+}
+
+func testDifficultyValue() {
+	difficulty := getDifficulty(0x181bc330)
+	if difficulty != "00000000000000001bc330000000000000000000000000000000000000000000" {
+		log.Fatal("Difficulty calculation failed: ", difficulty)
+	}
+}
+
+func solve(startNonce uint32) uint32 {
+	const NumIterations uint32 = 100_000_000
+	const NumConsumers uint32 = 12
+	const NumConsumerIterations = (NumIterations + NumConsumers - 1) / NumConsumers
+
+	header := BlockHeader{
+		Version:    0x20400000,
+		PrevBlock:  "00000000000000000006a4a234288a44e715275f1775b77b2fddb6c02eb6b72f",
+		MerkleRoot: "2dc60c563da5368e0668b81bc4d8dd369639a1134f68e425a9a74e428801e5b8",
+		Timestamp:  0x5DB8AB5E,
+		Bits:       0x17148EDF,
+	}
+
+	result := make(chan uint32)
+	difficulty := getDifficulty(header.Bits)
+	for consumer := uint32(0); consumer < NumConsumers; consumer++ {
+		from := startNonce + consumer*NumConsumerIterations
+		to := from + NumConsumerIterations
+		if to > startNonce+NumIterations {
+			to = startNonce + NumIterations
+		}
+
+		go func(header BlockHeader, from, to uint32) {
+			for nonce := from; nonce < to; nonce++ {
+				header.Nonce = nonce
+				hash := computeHash(header)
+				if hash < difficulty {
+					result <- header.Nonce
+					break
+				}
+			}
+			result <- 0
+		}(header, from, to)
+	}
+
+	for i := uint32(0); i < NumConsumers; i++ {
+		consumerResult := <-result
+		if consumerResult != 0 {
+			return consumerResult
+		}
+	}
+	return 0
+}
+
+func main() {
+	testExampleHeader()
+	testDifficultyValue()
+	log.Println("Finished testing..")
+
+	firstCase := solve(3_000_000_000) // 3060331852
+	log.Println("First case", firstCase)
+
+	for warmup := 0; warmup < 100; warmup++ {
+		rand.Int()
+	}
+	lookAhead := uint32(rand.Intn(1 << 30))
+	log.Println("lookAhead is", lookAhead)
+
+	secondCase := solve(firstCase + lookAhead)
+	log.Println("Second case", secondCase)
 }
